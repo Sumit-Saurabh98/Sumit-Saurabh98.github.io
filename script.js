66a80ee684940e60ed74cb46f5a7a5e96f1725aa
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-07-12 23:29:20 +0530

    updated till 12 july 2022

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ var resumeLink = document.querySelector("#resume-button-1");
 resumeLink.addEventListener("click", function (event) {
   event.preventDefault();
   window.open(
-    "https://www.dropbox.com/s/mtrij86ma0fcbwf/Sumit-Saurabh-Resume.pdf?dl=0",
+    "https://1drv.ms/b/s!AmhPXI-OneQSm2wTzafeg5Kbtkl4?e=oLTiUR",
     "_blank"
   );
   let link = document.createElement("a");
@@ -108,7 +108,7 @@ var resumeLink = document.querySelector("#resume-button-2");
 resumeLink.addEventListener("click", function (event) {
   event.preventDefault();
   window.open(
-    "https://www.dropbox.com/s/mtrij86ma0fcbwf/Sumit-Saurabh-Resume.pdf?dl=0",
+    "https://1drv.ms/b/s!AmhPXI-OneQSm2wTzafeg5Kbtkl4?e=oLTiUR",
     "_blank"
   );
   let link = document.createElement("a");
@@ -122,7 +122,7 @@ var resumeLink = document.querySelector("#resume-button-last");
 resumeLink.addEventListener("click", function (event) {
   event.preventDefault();
   window.open(
-    "https://www.dropbox.com/s/mtrij86ma0fcbwf/Sumit-Saurabh-Resume.pdf?dl=0",
+    "https://1drv.ms/b/s!AmhPXI-OneQSm2wTzafeg5Kbtkl4?e=oLTiUR",
     "_blank"
   );
   let link = document.createElement("a");
